9a76a72f11d53e95b6d812ee104a50c8d2e8c161
Магомед Муртузаев <58600545+Overlord5465@users.noreply.github.com>
2024-05-06 19:27:19 +0500

    Made "n. <task>" and "n. <answer>"

--- Magas_tasks.py
+++ Magas_tasks.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+ coding: utf-8
 
 from math import sqrt
 from functions import integr_lapl, local_lapl
@@ -63,10 +63,10 @@ win.mainloop()
 
 
 def task_1():
-    task = "Задача №1\n"
+    task = "1. "
     task += "Домашняя обезьянка бьет лапой по клавишам пишущей машинки "
     arr = ["А", "О", "Е", "И", "У", "Ю"]
-    answer = ""
+    answer = "1. "
     if (randint(0, 1)):
         task += "пять раз. Какова вероятность, что напечатанные буквы:\n"
         task += "\tа) составят имя её хозяина «"
@@ -74,7 +74,7 @@ def task_1():
         task += arr_1[randint(0, 9)] + "»;\n"
         task += "\tб) образуют слово, начинающееся с буквы «"
         task += arr[randint(0, 5)] + "»?\n"
-        answer = "a) 1/(33)^5\tb) 1/33"
+        answer += "a) 1/(33)^5\tb) 1/33\n"
     else:
         task += "шесть раз. Какова вероятность, что напечатанные буквы:\n"
         task += "\tа) составят имя её хозяйки «"
@@ -82,15 +82,15 @@ def task_1():
         task += arr_1[randint(0, 9)] + "»;\n"
         task += "\tб) образуют слово, начинающееся и заканчивающееся буквой «"
         task += arr[randint(0, 5)] + "»?\n"
-        answer = "a) 1/(33)^6\tb) 1/(33)^2"
+        answer += "a) 1/(33)^6\tb) 1/(33)^2\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_2():
-    task = "Задача №2\n"
+    task = "2. "
     task += "В группе из "
-    answer = ""
+    answer = "2. "
     x1 = randint(5, 10)
     x2 = randint(5, 10)
     task += "тридцати человек "
@@ -102,16 +102,16 @@ def task_2():
     task += "\tа) все пять — с выполненным полностью домашним заданием;\n"
     task += "\tб) две — с частично выполненным заданием и две — вообще без домашнего задания."
 
-    answer = "a) C(из " + str(x1) + " по 5) " + "\tb) C(из " + str(x2) + " по 2) * " + "C(из " + str(
-        30 - x1 - x2) + " по 2) * " + "C(из " + str(26) + " по 1)  /  C(из 30 по 5)"
+    answer += "a) C(из " + str(x1) + " по 5) " + "\tb) C(из " + str(x2) + " по 2) * " + "C(из " + str(
+        30 - x1 - x2) + " по 2) * " + "C(из " + str(26) + " по 1)  /  C(из 30 по 5)\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_3():
-    task = "Задача №3\n"
+    task = "3. "
     task += "Вероятность сняться в рекламе университета путей сообщения для первокурсника равна "
-    answer = ""
+    answer = "3. "
     x1 = randint(5, 9) / 10
     x2 = randint(1, 4) / 10
     task += str(x1) + ", для пятикурсника — "
@@ -121,17 +121,17 @@ def task_3():
     task += "\tб) только первокурсник;\n"
     task += "\tв) кто-нибудь из них?\n"
 
-    answer = "a) " + str(round(x1 * x2, 2)) + "\tб) " + str(round(x1 * (1 - x2), 2)) + "\tв) " + str(
-        round(x1 * x2 + x1 * (1 - x2) + (1 - x1) * x2, 2))
+    answer += "a) " + str(round(x1 * x2, 2)) + "\tб) " + str(round(x1 * (1 - x2), 2)) + "\tв) " + str(
+        round(x1 * x2 + x1 * (1 - x2) + (1 - x1) * x2, 2))+"\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_4():
-    task = "Задача №4\n"
+    task = "4. "
     task += "Два лаборанта делают измерения некоторой физической величины. "
     task += "Вероятность допустить ошибку при снятии показания для первого сотрудника равна "
-    answer = ""
+    answer = "4. "
     x1 = randint(1, 9) / 10
     q1 = round(1 - x1, 4)
     x2 = randint(1, 9) / 10
@@ -140,27 +140,27 @@ def task_4():
     task += str(
         x2) + ". Каждый лаборант сделал по два измерения.Какова вероятность, что ошибочных измерений у них поровну?\n"
 
-    answer = str(round(x1 * x1 * x2 * x2, 4) + round(x1 * q1 * x2 * q2, 4) + round(q1 * q1 * q2 * q2, 4))
+    answer += str(round(x1 * x1 * x2 * x2, 4) + round(x1 * q1 * x2 * q2, 4) + round(q1 * q1 * q2 * q2, 4))
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_5():
-    task = "Задача №5\n"
+    task = "5. "
     x = randint(10, 15)
     k = randint(3, x - 5)
     task += "В группе 14д механического факультета из 25 студентов " + str(
         x) + " не подготовились к занятию по математике."
     task += "Найти вероятность того, что " + str(k) + " случайно выбранных студентов оказались подготовленными."
-    answer = ""
+    answer = "5. "
 
-    answer = "C(из " + str(25 - x) + " по " + str(k) + ") / C(из 25 по " + str(k) + ")"
+    answer += "C(из " + str(25 - x) + " по " + str(k) + ") / C(из 25 по " + str(k) + ")\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_6():
-    task = "Задача №6\n"
+    task = "6. "
     ph1 = randint(1, 8) / 10
     ph2 = randint(1, 9 - ph1 * 10) / 10
     ph3 = round(1 - ph1 - ph2, 2)
@@ -175,7 +175,7 @@ def task_6():
         p2) + ", для мориона — " + str(p3) + ". "
     task += "Не искушенная в ювелирном искусстве юная барышня выбирает украшение случайным образом."
     task += " Какова вероятность того, что оно не будет содержать примеси двойника?"
-    answer = ""
+    answer = "6. "
 
     pah1 = 1 - p1
     pah2 = 1 - p2
@@ -183,13 +183,13 @@ def task_6():
 
     pa = ph1 * pah1 + ph2 * pah2 + ph3 * pah3
 
-    answer = str(round(pa, 2))
+    answer += str(round(pa, 2))+"\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_7():
-    task = "Задача №7\n"
+    task = "7. "
     n1 = randint(5, 12) * 90
     n2 = randint(5, 12) * 90
     n3 = 3000 - n1 - n2
@@ -202,7 +202,7 @@ def task_7():
         p3) + " соответственно."
     task += "Беспечный покупатель купил ботинки наудачу. Через неделю у правого ботинка отвалилась подошва."
     task += " На какой фабрике вероятнее всего были сделаны эти ботинки?"
-    answer = ""
+    answer = "7. "
 
     ph1 = n1 / 3000
     ph2 = n2 / 3000
@@ -214,28 +214,28 @@ def task_7():
     ph2a = p2 * ph2 / pa
     ph3a = p3 * ph3 / pa
 
-    answer = str(round(max(ph1a, ph2a, ph3a), 2))
+    answer += str(round(max(ph1a, ph2a, ph3a), 2))+"\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_8():
-    task = "Задача №8\n"
+    task = "8. "
     p = randint(1, 9) / 10
     task += "Событие В произойдет в случае, если событие А наступит не менее четырех раз. Найти вероятность события"
     task += " В, если производится пять независимых испытаний, в каждом из которых вероятность совершения А равна " + str(
         p) + "."
 
-    answer = ""
+    answer = "8. "
 
-    answer = str("C(из 5 по 4) * " + str(p) + "^4 * " + str(1 - p) + "+ C(из 5 по 5) * " + str(p) + "^5 = " + str(
-        round(pow(p, 5) + 4 * pow(p, 4), 5)))
+    answer += str("C(из 5 по 4) * " + str(p) + "^4 * " + str(1 - p) + "+ C(из 5 по 5) * " + str(p) + "^5 = " + str(
+        round(pow(p, 5) + 4 * pow(p, 4), 5)))+"\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_9():
-    task = "Задача №9\n"
+    task = "9. "
     n = 100
     k = 50
     k1 = 100
@@ -246,38 +246,37 @@ def task_9():
     task += "\tа) половина деталей номинальных размеров;\n"
     task += "\tб) не менее половины таких деталей."
 
-    answer = ""
+    answer = "9. "
 
     x = (k - n * p) / sqrt(n * p * (1 - p))
     x2 = (k1 - n * p) / sqrt(n * p * (1 - p))
 
-    answer = "a) " + str(round(float(local_lapl(round(x, 1))) / sqrt(n * p * (1 - p)), 3))
-    answer += "\tб)" + str(round(float(integr_lapl(round(x2, 2))) - float(integr_lapl(round(x, 2))), 3))
+    answer += "a) " + str(round(float(local_lapl(round(x, 1))) / sqrt(n * p * (1 - p)), 3))
+    answer += "\tб)" + str(round(float(integr_lapl(round(x2, 2))) - float(integr_lapl(round(x, 2))), 3))+"\n"
 
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_10():
-    task = "Задача №10\n"
+    task = "10. "
     n = randint(3, 7) * 100
     p = randint(3, 7) / 1000
     k = 5
     task += "Среди семян ржи " + str(p * 100) + "% семян сорняков. Какова вероятность при случайном наборе " + str(
         n) + " семян обнаружить пять семян сорняков?"
 
-    answer = ""
+    answer = "10. "
 
     x = (k - n * p) / sqrt(n * p * (1 - p))
-    print(str(n) + "\t" + str(p) + "\t" + str(x))
-    answer = str(round(float(local_lapl(round(x, 1))) / sqrt(n * p * (1 - p)), 3))
+    answer += str(round(float(local_lapl(round(x, 1))) / sqrt(n * p * (1 - p)), 3)) + "\n"
 
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_11():
-    task = "Задача №11\n"
+    task = "11. "
 
     p = randint(1, 9) / 10
     q = round(1 - p, 1)
@@ -288,7 +287,7 @@ def task_11():
     task += " оказывается стандартным, контролер берет следующее и т. д. Составить ряд распределения числа проверенных"
     task += " изделий. Найти М(Х), D(X), σ(X), F(X) этой случайной величины. Построить график F(X)."
 
-    answer = ""
+    answer = "11. "
 
     p1 = round(q, 1)
     p2 = round(p * q, 2)
@@ -296,7 +295,7 @@ def task_11():
     p4 = round(p * p * p * q, 4)
     p5 = round(p * p * p * p, 4)
 
-    answer = "Ряд распределения: \nxi\t1\t2\t3\t4\t5\n"
+    answer += "Ряд распределения: \nxi\t1\t2\t3\t4\t5\n"
     answer += "pi\t" + str(p1) + "\t" + str(p2) + "\t" + str(p3) + "\t" + str(p4) + "\t" + str(p5) + "\n\n"
     M = 1 * p1 + 2 * p2 + 3 * p3 + 4 * p4 + 5 * p5
     answer += "M(X) = " + str(M) + "\t"
@@ -312,13 +311,13 @@ def task_11():
     F += "\t| " + str(round(p1 + p2 + p3 + p4, 4)) + ", 4<x≤5\n"
     F += "\t| 1\t, 5<x\n"
     F += "\t‾‾‾‾‾‾‾‾‾‾"
-    answer += F
+    answer += F+"\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_12():
-    task = "Задача №12\n"
+    task = "12. "
 
     n = 3
     p = randint(1, 9) / 10
@@ -328,26 +327,26 @@ def task_12():
         p) + " . Составить"
     task += "ряд распределения числа появлений события в трех опытах. Найти M(X) и D(X) этой случайной величины."
 
-    answer = ""
+    answer = "12. "
 
     p0 = round(q * q * q, 3)
     p1 = round(3 * p * q * q, 3)
     p2 = round(3 * p * p * q, 3)
     p3 = round(p * p * p, 3)
 
-    answer = "Ряд распределения: \nxi\t0\t1\t2\t3\n"
+    answer += "Ряд распределения: \nxi\t0\t1\t2\t3\n"
     answer += "pi\t" + str(p0) + "\t" + str(p1) + "\t" + str(p2) + "\t" + str(p3) + "\n\n"
     M = round(n * p, 1)
     answer += "M(X) = " + str(M) + "\t"
     D = round(n * p * q, 2)
-    answer += "D(X) = " + str(D) + "\t"
+    answer += "D(X) = " + str(D) + "\n"
 
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_13():
-    task = "Задача №13\n"
+    task = "13. "
     r = randint(1, 3)
     n = 0
     if r == 1:
@@ -362,20 +361,20 @@ def task_13():
     task += "Устройство содержит " + str(n) + " одинаково надежных элементов, вероятность отказа каждого из них равна "
     task += str(p) + ". Составить ряд распределения числа отказавших элементов. Найти M(X) этой случайной величины."
 
-    answer = ""
+    answer = "13. "
 
-    answer = "Ряд распределения: \nxi\t0\t1\t\t2\t\t\t...\tk\t\t\t...\tn\n"
+    answer += "Ряд распределения: \nxi\t0\t1\t\t2\t\t\t...\tk\t\t\t...\tn\n"
     answer += "pi\te^(-λ)\tλ*e^(-λ)\t((λ^2)/2)*e^(-λ)\t...\t((λ^k)/k)*e^(-λ)\t...\t((λ^n)/n)*e^(-λ)\n\n"
     answer += ",где n = " + str(n) + "\tp = " + str(p) + "\tλ = n * p = " + str(n * p) + "\n\n"
     M = n * p
-    answer += "M(X) = n * p = " + str(M) + "\t"
+    answer += "M(X) = n * p = " + str(M) + "\n"
 
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_14():
-    task = "Задача №14\n"
+    task = "14. "
 
     task += "Независимые случайные величины X и Y заданы таблицами распределений.\n"
     task += "Найти:\n"
@@ -398,7 +397,7 @@ def task_14():
     task += f'Y:\t{yi[0]}\t{yi[1]}\n'
     task += f'P:\t{pyi[0]}\t{pyi[1]}\n'
 
-    answer = ""
+    answer = "14. "
 
     answer += "1) M(X) = 0.1\tD(X) = 1.89\tM(Y) = 3.2\tD(Y) = 0.96\n"
 
@@ -420,14 +419,14 @@ def task_14():
 
     answer += "\n3) M(Z1) = M(2X+Y) = 2 * M(X) + M(Y) = 2 * 0.1 + 3.2 = 3.4\n   D(Z1) = D(2X+Y) = 4 * D(X) + D(Y) = 4 * 1.89 + 0.96 = 8.52\n"
     answer += "   M(Z2) = M(X*Y) = M(X) * M(Y) = 0.1 * 3.2 = 0.32\n"
-    answer += "   D(Z2) = M( (Z2)^2 ) - M(Z2)^2 = 16 * 0.36 + 4 * 0.24 + 4 * 0.04 + 16 * 0.18 + 64 * 0.18 - 0.1024 = 21.1776"
+    answer += "   D(Z2) = M( (Z2)^2 ) - M(Z2)^2 = 16 * 0.36 + 4 * 0.24 + 4 * 0.04 + 16 * 0.18 + 64 * 0.18 - 0.1024 = 21.1776\n"
 
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_15():
-    task = "Задача №15\n"
+    task = "15. "
 
     a = randint(11, 14) / 10
     b = randint(15, 19) / 10
@@ -445,7 +444,7 @@ def task_15():
     task += "\t4) найти Р(α < X < β) для данных α, β.\n"
     task += F
 
-    answer = ""
+    answer = "15. "
 
     f = "\t__________\n"
     f += "\t| 0\t\t, x≤1\n"
@@ -453,10 +452,10 @@ def task_15():
     f += "\t| 0\t\t, 2<x\n"
     f += "\t‾‾‾‾‾‾‾‾‾‾\n"
 
-    answer = "1)" + f
+    answer += "1)" + f
     answer += "3) M(x) = 1.58\tD(x) = 0.076\tσ(x) = 0.076\n"
     answer += "4) P(" + str(a) + "<x<" + str(b) + ") = F(" + str(b) + ")-F(" + str(a) + ") = " + str(
-        round(((b * b - b) - (a * a - a)) / 2, 3))
+        round(((b * b - b) - (a * a - a)) / 2, 3)) + "\n"
 
     task_and_answer = [task, answer]
     return task_and_answer
@@ -498,7 +497,7 @@ def task_16():
 
 
 def task_16():
-    task = "Задача №16\n"
+    task = "16. "
 
     a = randint(-5, -3)
     b = randint(1, 100)
@@ -518,7 +517,7 @@ def task_16():
     task += "\t5) найти М(Х), D(X), σ(X).\n"
     task += f
 
-    answer = ""
+    answer = "16. "
 
     F = "\t__________\n"
     F += "\t| 0\t\t, x≤-6\n"
@@ -527,18 +526,18 @@ def task_16():
     F += "\t| 1\t\t, 0<x\n"
     F += "\t‾‾‾‾‾‾‾‾‾‾\n"
 
-    answer = "1) свойство выполняется\n"
+    answer += "1) свойство выполняется\n"
     answer += "3) " + F
     answer += "4) P(" + str(a) + "<x<" + str(b) + ") = F(" + str(b) + ")-F(" + str(a) + ") = " + str(
         round(1 - ((a * a + 12 * a + 36) / 18), 3)) + "\n"
-    answer += "5) M(X) = -3\tD(X) = 1.5\tσ(X) = 1.22"
+    answer += "5) M(X) = -3\tD(X) = 1.5\tσ(X) = 1.22\n"
 
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_17():
-    task = "Задача №17\n"
+    task = "17. "
 
     n = 5 if randint(0, 1) else 10
     arr = ["одну минуту", "две минуты", "три минуты", "четыре минуты"]
@@ -549,7 +548,7 @@ def task_17():
     task += " Какова вероятность того, что он подошел не ранее чем через минуту после ухода предыдущего трамвая, "
     task += f"но не позднее, чем за {s} до отхода следующего?"
 
-    answer = ""
+    answer = "17. "
 
     answer += str((n - 2 - t) / 10)
     task_and_answer = [task, answer]
@@ -557,7 +556,7 @@ def task_17():
 
 
 def task_18():
-    task = "Задача №18\n"
+    task = "18. "
 
     a0 = randint(1, 10)
     b0 = randint(13, 24)
@@ -567,15 +566,15 @@ def task_18():
     task += f'Все значения равномерно распределенной случайной величины X лежат на отрезке [{a0}; {b0}]. '
     task += f'Какова вероятность события: X принадлежит [{a1}; {b1}]?'
 
-    answer = ""
+    answer = "18. "
 
-    answer += str(round((b1 - a1) / (b0 - a0), 3))
+    answer += str(round((b1 - a1) / (b0 - a0), 3)) + "\n"
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_19():
-    task = "Задача №19\n"
+    task = "19. "
 
     m = randrange(35, 45, 5)
     q = 5
@@ -586,7 +585,7 @@ def task_19():
     task += f'ожиданием m = {m} и σ = 5. Построить график плотности вероятности f(x) и '
     task += f'сравнить вероятности попадания X в интервалы (0; {x1}) и ({x2}; 55).\n'
 
-    answer = ""
+    answer = "19. "
 
     answer += f'Точка максимума: ({m}, 1/({q}*√2π)) ; Точки перегиба: ({m - q}, 1/({q}*√2πe)), (({m + q}, 1/({q}*√2πe)))\n'
     P1 = float(integr_lapl((x1 - m) / 5) - integr_lapl((-m) / 5))
@@ -595,13 +594,13 @@ def task_19():
     P2 = round(P2, 4)
     answer += f'P(0<X<{x1}) = {P1}\t'
     answer += f'P({x2}<X<55) = {P2}\n'
-    answer += f'P(0<X<{x1})<P({x2}<X<55)' if P1 < P2 else f'P(0<X<{x1})>P({x2}<X<55)'
+    answer += f'P(0<X<{x1})<P({x2}<X<55)' if P1 < P2 else f'P(0<X<{x1})>P({x2}<X<55)\n'
     task_and_answer = [task, answer]
     return task_and_answer
 
 
 def task_20():
-    task = "Задача №20\n"
+    task = "20. "
 
     n = randrange(35, 45, 5)
     m0 = 50
@@ -615,7 +614,7 @@ def task_20():
     task += f'измерительная система выходит из строя. С какой вероятностью измерительная система проработает до выхода '
     task += f'из строя от {a} до {b} ч? \n'
 
-    answer = ""
+    answer = "20. "
 
     m = n * m0
     D = n * y0 * y0
@@ -626,13 +625,13 @@ def task_20():
     answer += f'm = M(T) = M(ΣTi) = n * m0 = {m}\t'
     answer += f'D(T) = D(ΣTi) = n * σ0^2 = {D} \t'
     answer += f'σ = √D(T) = {y} \n\n'
-    answer += f'P({a}<X<{b}) = Ф( ({b}-{m}) / {y}) - Ф( ({a}-{m}) / {y}) = {P}'
+    answer += f'P({a}<X<{b}) = Ф( ({b}-{m}) / {y}) - Ф( ({a}-{m}) / {y}) = {P}\n'
 
     task_and_answer = [task, answer]
     return task_and_answer
 
 
-# check = task_14()
+#check = task_20()
 
-# print(check[0])
-# print(check[1])
+#print(check[0])
+#print(check[1])
